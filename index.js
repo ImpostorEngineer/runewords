139ac6dc9fa651d12c823c2e3548fdb660e7ebe1
GereksizPosta <posta.gereksiz@gmail.com>
2020-12-21 17:25:15 -0500

    New Updates

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const express = require('express');
 const cors = require('./cors');
 const app = express();
 
-app.use(cors.allowCors());
+app.use(cors.allowCors);
 app.use(express.json());
 app.use(express.static("data"));
 app.use(express.static("styles"));
