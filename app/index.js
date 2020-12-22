a2df4b1458c9a6ebb84a0a64e8c07519225e3627
GereksizPosta <posta.gereksiz@gmail.com>
2020-12-22 15:23:45 -0500

    CORS update

--- app/index.js
+++ app/index.js
@@ -2,31 +2,13 @@ const express = require('express');
 const api = require('./app');
 const app = express();
 
-const allowCors = fn => async (req, res) => {
-  res.setHeader('Access-Control-Allow-Credentials', true)
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  // another common pattern
-  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  )
-  if (req.method === 'OPTIONS') {
-    res.status(200).end()
-    return
-  }
-  return await fn(req, res)
-}
-
 app.use(express.static("client"));
-app.use('/api/', allowCors(api));
+app.use('/api/', api);
 app.use(express.json());
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '../client/index.html');
 })
-
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
